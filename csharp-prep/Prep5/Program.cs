91d899354d31d24d868b33f7e4f42e2821cecd3f
nasbf <mo0ma@hotmail.com>
2024-09-21 08:16:57 -0500

    finish preps2

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,12 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
+        /* primero se llama el metodo no al final. 
+        cuando creo el metodo identifico si es string o int o void(mix), 
+        para llamar el metodo si necesito el resultado debo crear la variable por ejemplo, cree la variable name y su 
+        resultado es lo que regresa el metodo PromptUserName.
+        Los argumentos son las variable que he creado fuera del metodo en este caso (name y x) los uso en el metodo 
+        DisplayResult.  */
+
+
         DisplayMessage ();
         string name = PromptUserName ();
         int number = PromptUserNumber ();
         double x = SquareNumber (number);
+        DisplayResult (name, x);
 
-                Console.WriteLine($"{name}, the square of your number is {x}");
+        
         
 
 
@@ -36,6 +45,11 @@ class Program
                 return x;
             }
         
+        static void DisplayResult(string name, double x)
+            {
+                Console.WriteLine($"{name}, the square of your number is {x}");
+            }
+
 
     }
 }
