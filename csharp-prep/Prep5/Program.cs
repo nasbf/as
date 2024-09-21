480e0c54cae43793ecd625662fae71d5613043b0
nasbf <mo0ma@hotmail.com>
2024-09-21 08:00:28 -0500

    finish preps

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayMessage ();
+        string name = PromptUserName ();
+        int number = PromptUserNumber ();
+        double x = SquareNumber (number);
+
+                Console.WriteLine($"{name}, the square of your number is {x}");
+        
+
+
+        static void DisplayMessage()
+        {
+           Console.WriteLine("Welcome to the program");
+        }
+        
+        static string PromptUserName()
+        {  
+            Console.WriteLine("Please enter your name:  ");
+            string userName = Console.ReadLine();
+            return (userName);
+        }   
+        static int PromptUserNumber()
+            {  
+                Console.WriteLine("Please enter your number:  ");
+                int userNumber = int.Parse(Console.ReadLine());
+                return userNumber;
+            }   
+        static double SquareNumber(int number)
+            {
+                double x = Math.Pow(number, 2);
+                return x;
+            }
+        
+
     }
 }
