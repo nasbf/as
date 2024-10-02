27ac00683e047f2720508b0ec423a212691d0cec
nasbf <mo0ma@hotmail.com>
2024-10-01 19:26:32 -0500

    Learning03

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,60 @@
+using System;
+
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction ()
+    {
+        _top = 1;
+        _bottom = 1;
+
+    }
+
+    public Fraction (int WholeNumber)
+    {
+        _top = WholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction (int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+       
+    }
+    public int GetTop()
+    {
+        return _top;
+    }
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetTop (int top)
+    {
+        _top = top;
+    }
+    public void SetBottom (int bottom)
+    {
+        _bottom = bottom;
+    }
+    public string GetFractionString ()
+    {
+         return($"{_top}/{_bottom}");
+    }
+public double GetDecimal ()
+    {
+         return(double)_top/_bottom;
+    }
+        
+    
+    
+    
+
+
+
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction result = new Fraction ();
+        Console.WriteLine(result.GetFractionString ());
+        Console.WriteLine(result.GetDecimal ());
+        Console.WriteLine();
+
+        Fraction result1 = new Fraction (8);
+        Console.WriteLine(result1.GetFractionString ());
+        Console.WriteLine(result1.GetDecimal ());
+        Console.WriteLine();
+        
+        Fraction result2 = new Fraction (5,3);
+        Console.WriteLine(result2.GetFractionString ());
+        Console.WriteLine(result2.GetDecimal ());
+        Console.WriteLine();
+        
+        Fraction result3 = new Fraction (6,2);
+        Console.WriteLine(result3.GetFractionString ());
+        Console.WriteLine(result3.GetDecimal ());
+        Console.WriteLine();
+        
     }
 }
